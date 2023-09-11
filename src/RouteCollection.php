bb3772afc758ed7a49f809bd1f6842c44b67b8f9
Alexis Cortes <alexis.cortes.0199@gmail.com>
2023-09-11 16:14:19 -0600

    feat(RouteCollection): Add module that represents a collection of Route objects

---
+++ src/RouteCollection.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Yolanda\Routing;
+
+use SplObjectStorage;
+
+/**
+ * Represents a collection of Route objects.
+ *
+ */
+class RouteCollection
+{
+    /**
+     * Collection
+     *
+     * @var SplObjectStorage
+     */
+    private SplObjectStorage $storage;
+
+    /**
+     * Initializes a new RouteCollection.
+     */
+    public function __construct()
+    {
+        $this->storage  =   new SplObjectStorage();
+    }
+
+    /**
+     * Attach a Route to the collection.
+     *
+     * @param Route $route
+     * @return $this
+     */
+    public function attach(Route $route): self
+    {
+        $this->storage->attach($route, null);
+
+        return $this;
+    }
+
+
+    /**
+     * Fetch all routes stored on this collection of routes and return it.
+     *
+     * @return Route[]
+     */
+    public function all(): array
+    {
+        $routes =   [];
+        foreach ($this->storage as $value) {
+            $routes[]   =   $value;
+        }
+
+        return $routes;
+    }
+}
