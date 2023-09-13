2cd0d5c1cee12a08e81940360c377be55e2c3b62
Alexis Cortes <alexis.cortes.0199@gmail.com>
2023-09-12 21:30:30 -0600

    feat(RouteRegister): add module RouteRegister

---
+++ src/RouteRegister.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Yolanda\Routing;
+
+/**
+ *
+ */
+class RouteRegister
+{
+
+    /**
+     * @var string
+     */
+    private string $uri;
+    /**
+     * @var string
+     */
+    private string $name;
+    /**
+     * @var array
+     */
+    private array $method;
+    /**
+     * @var mixed
+     */
+    private mixed $callback;
+    /**
+     * @var RouteCollection
+     */
+    private RouteCollection $collection;
+
+    /**
+     * @param RouteCollection $collection
+     */
+    public function __construct(RouteCollection $collection)
+    {
+        $this->collection = $collection;
+        $this->init();
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function name(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @param string $uri
+     * @param array|callable|string|null $callback
+     * @return $this
+     */
+    public function get(string $uri, array|callable|null|string $callback): self
+    {
+        return $this->addRoute($uri, 'GET', $callback);
+    }
+
+    /**
+     * @param string $uri
+     * @param array|callable|string|null $callback
+     * @return $this
+     */
+    public function post(string $uri, array|callable|null|string $callback): self
+    {
+        return $this->addRoute($uri, 'POST', $callback);
+    }
+
+    /**
+     * @param string $uri
+     * @param array|callable|string|null $callback
+     * @return $this
+     */
+    public function put(string $uri, array|callable|null|string $callback): self
+    {
+        return $this->addRoute($uri, 'PUT', $callback);
+    }
+
+    /**
+     * @param string $uri
+     * @param array|callable|string|null $callback
+     * @return $this
+     */
+    public function delete(string $uri, array|callable|null|string $callback): self
+    {
+        return $this->addRoute($uri, 'DELETE', $callback);
+    }
+
+    /**
+     * @param string $uri
+     * @param string $method
+     * @param array|callable|string|null $callback
+     * @return $this
+     */
+    public function addRoute(string $uri, string $method, array|callable|null|string $callback): self
+    {
+        if (in_array($method, $this->method)){
+            return $this;
+        }
+
+        $this->uri  =   $uri;
+        $this->method[]   =   $method;
+        $this->callback =   $callback;
+
+        return $this;
+    }
+
+    /**
+     * @return void
+     */
+    public function done(): void
+    {
+        $route  =   new Route(
+            $this->name,
+            $this->uri,
+            $this->method,
+            $this->callback
+        );
+        $this->collection->attach($route);
+        $this->init();
+    }
+
+
+    /**
+     * @return void
+     */
+    private function init(): void
+    {
+        $this->uri  =   '';
+        $this->name =   '';
+        $this->method  =   [];
+        $this->callback =   null;
+    }
+}
