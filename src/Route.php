9678e14b3ca644692f5ddba928d1c78556451d2b
Alexis Cortes <alexis.cortes.0199@gmail.com>
2023-09-12 20:44:00 -0600

    feat(Route): add new feactures for route module

--- src/Route.php
+++ src/Route.php
@@ -55,9 +55,9 @@ class Route
     /**
      * Callback or action from Route
      *
-     * @var mixed|callable
+     * @var mixed
      */
-    private mixed $callback;
+    private  mixed $callback;
 
     /**
      * Args of callback action from Route
@@ -67,21 +67,30 @@ class Route
     private array $argsCallback;
 
     /**
+     * @var array
+     */
+    private array $callbackInfo;
+
+    /**
      * @param string $name
      * @param string $uri
-     * @param array $method
-     * @param callable $callback
+     * @param array|string $method
+     * @param array|callable|null|string $callback
      */
-    public function __construct(string $name, string $uri, array $method, callable $callback)
+    public function __construct(string $name, string $uri, array | string $method,  array|callable|null|string $callback)
     {
         $uri = $this->cleanRequestUri($uri);
         $this->name = $name;
         $this->uri = $uri;
-        $this->method = $method;
+        $this->method = is_array($method) ? $method : [$method];
         $this->callback = $callback;
-        $this->parameters = $this->extractParametersFromUri($uri);
         $this->parametersWithValues = [];
         $this->argsCallback = [];
+        $this->callbackInfo =   [];
+
+
+        $this->parameters   =   $this->extractParametersFromUri($uri);
+        $this->callbackInfo =   $this->parseCallbackInfo($callback);
 
         foreach ($this->parameters as $parameter) {
             $this->parametersWithValues[$parameter] = null;
@@ -129,6 +138,16 @@ class Route
         return $this->parametersWithValues;
     }
 
+    /**
+     * @param bool|string $parameterName
+     * @param mixed $value
+     * @return void
+     */
+    public function setParameterValue(bool|string $parameterName, mixed $value): void
+    {
+        $this->parametersWithValues[$parameterName] = $value;
+    }
+
 
     /**
      * Checks if the provided URI and HTTP method match the route's criteria.
@@ -165,7 +184,7 @@ class Route
 
             if ($parameterName = $this->getParameterFromSegment($uriSegments[$i])) {
                 if (in_array($parameterName, $this->parameters)) {
-                    $this->parametersWithValues[$parameterName] = $uriRequestSegments[$i];
+                    $this->setParameterValue($parameterName, $uriRequestSegments[$i]);
                     continue;
                 }
             }
@@ -176,79 +195,10 @@ class Route
     }
 
     /**
-     * Sets the arguments for the callback based on request parameters.
-     *
-     * @throws InvalidUrlParametersException if the number of URL parameters doesn't match the callback.
-     * @throws ReflectionException if there is an issue with obtaining callback reflection.
-     * @throws InvalidFunctionParametersException if callback parameters are missing in the request.
-     */
-    public function setArgsCallback(): void
-    {
-        $callable = $this->callback;
-
-        $reflection = new ReflectionFunction($callable);
-        $parameters = $reflection->getParameters();
-
-        $filteredFunctionParams = array_filter($parameters, fn ($param) =>
-            !$param->allowsNull() && !$param->isOptional() && !$param->isDefaultValueAvailable()
-        );
-
-        $filteredRequestParams = array_filter($this->parametersWithValues, fn ($param) => $param !== null);
-
-        if (count($filteredRequestParams) !== count($filteredFunctionParams)) {
-            throw new InvalidUrlParametersException($this->name);
-        }
-
-        $this->argsCallback = [];
-
-        if (empty($filteredFunctionParams) && empty($filteredRequestParams)) {
-            return;
-        }
-
-        foreach ($filteredFunctionParams as $param) {
-            if (!array_key_exists($param->name, $filteredRequestParams)) {
-                throw new InvalidFunctionParametersException($reflection->name);
-            }
-        }
-
-        $this->argsCallback = $filteredRequestParams;
-    }
-
-
-    /**
-     * Verifies that the provided callback is callable.
-     *
-     * @throws InvalidFunctionException if the callback is not callable.
-     */
-    public function prepareCallback(): void
-    {
-        $callable   =   $this->callback;
-        if (false === is_callable($callable)){
-            throw new InvalidFunctionException($callable);
-        }
-    }
-
-    /**
-     * Calls the stored callback function with the provided arguments.
-     *
-     * @return mixed The result of the callback function.
-     */
-    public function runCallback(): mixed
-    {
-        $callback = $this->callback;
-        $args = $this->argsCallback;
-
-        $result = call_user_func_array($callback, $args);
-
-        return $result;
-    }
-
-
-    /**
-     * @throws InvalidFunctionException
+     * @return mixed
+     * @throws InvalidFunctionParametersException
      * @throws InvalidUrlParametersException
      * @throws ReflectionException
-     * @throws InvalidFunctionParametersException
      */
     public function dispatch(): mixed
     {
@@ -260,5 +210,99 @@ class Route
 
         return $result;
     }
+
+    /**
+     * Sets the arguments for the callback based on request parameters.
+     *
+     * @throws InvalidUrlParametersException if the number of URL parameters doesn't match the callback.
+     * @throws ReflectionException if there is an issue with obtaining callback reflection.
+     * @throws InvalidFunctionParametersException if callback parameters are missing in the request.
+     */
+    private function setArgsCallback(): void
+    {
+        $callable = $this->callback;
+        $this->argsCallback =   [];
+
+        $filteredRequestParams = array_filter($this->parametersWithValues, fn ($param) => $param !== null);
+        $info       =   $this->callbackInfo;
+
+        switch ($info['type']){
+            case self::$IS_ARRAY_OBJECT:
+            case self::$IS_ARRAY_NAMESPACE_CLASS:
+            case self::$IS_NAMESPACE_STRING_AND_METHOD:
+                $reflection =   new \ReflectionClass($info['class']);
+                $parameters =   $reflection->getMethod($info['method'])->getParameters();
+                break;
+            case self::$IS_CALLBACK:
+                $reflection = new ReflectionFunction($callable);
+                $parameters = $reflection->getParameters();
+                break;
+            default:
+                throw new InvalidFunctionParametersException('');
+        }
+
+        $filteredFunctionParams = array_filter($parameters, fn ($param) =>
+            !$param->allowsNull() && !$param->isOptional() && !$param->isDefaultValueAvailable()
+        );
+
+        if (count($filteredFunctionParams) === 0){
+            return;
+        }
+
+        if (count($filteredFunctionParams) > count($filteredRequestParams)) {
+            /*IMPLEMENT LOGIC FOR BODY DATA*/
+            throw new InvalidUrlParametersException($this->name);
+        }
+
+        foreach ($filteredFunctionParams as $param) {
+            if (!array_key_exists($param->name, $filteredRequestParams)) {
+                throw new InvalidFunctionParametersException($reflection->name, $param->name);
+            }
+            $this->argsCallback[]   =   $filteredRequestParams[$param->name];
+        }
+
+    }
+
+
+    /**
+     * Verifies that the provided callback is callable.
+     *
+     */
+    private function prepareCallback(): void
+    {
+        $callable   =   $this->callback;
+        $info       =   $this->parseCallbackInfo($callable);
+        $this->callbackInfo =   $info;
+
+    }
+
+    /**
+     * Calls the stored callback function with the provided arguments.
+     *
+     * @return mixed The result of the callback function.
+     * @throws InvalidFunctionParametersException
+     */
+    private function runCallback(): mixed
+    {
+        $callback = $this->callback;
+        $args = $this->argsCallback;
+
+        $info   =   $this->callbackInfo;
+        switch ($info['type']){
+            case self::$IS_ARRAY_OBJECT:
+            case self::$IS_ARRAY_NAMESPACE_CLASS:
+            case self::$IS_NAMESPACE_STRING_AND_METHOD:
+                $result = call_user_func_array([$info['object'], $info['method']], $args);
+                break;
+            case self::$IS_CALLBACK:
+                $result = call_user_func_array($callback, $args);
+                break;
+            default:
+                throw new InvalidFunctionParametersException('', '');
+        }
+
+        return $result;
+    }
+
 
 }
