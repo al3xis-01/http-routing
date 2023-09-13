2fc8d874d435e380609367ab0dad941a0a800c05
Alexis Cortes <alexis.cortes.0199@gmail.com>
2023-09-12 20:46:25 -0600

    feat(InvalidFunctionParametersException): add parameter name to exeception

--- src/Exceptions/InvalidFunctionParametersException.php
+++ src/Exceptions/InvalidFunctionParametersException.php
@@ -4,7 +4,7 @@ namespace Yolanda\Routing\Exceptions;
 
 class InvalidFunctionParametersException extends \Exception
 {
-    public function __construct(string $function) {
-        parent::__construct(sprintf('Function %s parameters do not match callback parameters.', $function));
+    public function __construct(string $function, string $nameParameter = '') {
+        parent::__construct(sprintf('Function %s parameters (%s) do not match callback parameters (%s).', $function, $nameParameter, ''));
     }
 }
