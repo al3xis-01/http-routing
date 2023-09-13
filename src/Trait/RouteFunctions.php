18ccd5b60d748764ac74ff4d970f27b15453c762
Alexis Cortes <alexis.cortes.0199@gmail.com>
2023-09-13 01:09:08 -0600

    feat(RouteFunctions): Add instantiates a class or object and saves it in the info variable

--- src/Trait/RouteFunctions.php
+++ src/Trait/RouteFunctions.php
@@ -4,8 +4,16 @@ namespace Yolanda\Routing\Trait;
 
 trait RouteFunctions
 {
+
+    public static int $IS_ARRAY_OBJECT   =   1;
+    public static int $IS_ARRAY_NAMESPACE_CLASS   =   2;
+    public static int $IS_NAMESPACE_STRING_AND_METHOD   =   3;
+    public static int $IS_CALLBACK   =   4;
+    public static int $IS_UNKNOWN   =   -1;
+
     private function cleanRequestUri(string $requestUri): string {
 
+        $requestUri =   rawurldecode($requestUri);
 
         if (str_starts_with($requestUri, '/')) {
             $requestUri = substr($requestUri, 1);
@@ -27,9 +35,15 @@ trait RouteFunctions
 
     private function getParameterFromSegment(string $segment): bool | string
     {
+        $segment    =   rawurldecode($segment);
         if (str_starts_with($segment, '{') && str_ends_with($segment, '}')){
             return trim($segment, '{}');
         }
+
+        if (str_starts_with($segment, ':')){
+            return trim($segment, ':');
+        }
+
         return false;
     }
 
@@ -40,13 +54,63 @@ trait RouteFunctions
         $parameters   =   [];
 
         foreach ($uriSegments as $segment) {
-
             if ($parameterName = $this->getParameterFromSegment($segment)) {
                 $parameters[] = $parameterName;
             }
         }
 
         return $parameters;
+    }
+
+    private function parseCallbackInfo( array|callable|null|string $callback): array
+    {
+        $info   =   [];
+        $info['type']   =   self::$IS_UNKNOWN;
+
+        if (is_array($callback) && count($callback) === 2){
+            $resource   =   $callback[0];
+            $method     =   $callback[1];
+
+            if (is_object($resource)){
+                $info['type']       =   self::$IS_ARRAY_OBJECT;
+                $info['description'] = 'Array object';
+                $info['class']  =   get_class($resource);
+                $info['object'] =   $resource;
+                $info['method'] =   $method;
+                return $info;
+
+            }
+
+            if (is_string($resource)){
+                $info['type']           =   self::$IS_ARRAY_NAMESPACE_CLASS;
+                $info['description']   =   'Array namespace class';
+                $info['class']  =   $resource;
+                $info['object'] =   new $resource();
+                $info['method'] =   $method;
+                return $info;
+            }
+        }
+
+        if (is_string($callback) && str_contains($callback, '::')){
+            list($class, $method)   =   explode('::', $callback);
+
+            $info['type']           =   self::$IS_NAMESPACE_STRING_AND_METHOD;
+            $info['description']   =   'Namespace string and method';
+            $info['class']  =   $class;
+            $info['object'] =   new $class();
+            $info['method'] =   $method;
+            return $info;
+        }
+
+        if (is_callable($callback)){
+            $info['type']           =   self::$IS_CALLBACK;
+            $info['description']   =   'Anonymous function or callback';
+            return $info;
+        }
+
+
+
+        return $info;
     }
     
 }
